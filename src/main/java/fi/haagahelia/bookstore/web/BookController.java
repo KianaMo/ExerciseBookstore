747240dcfc6f22c9badd6c51f93d39b75313cba3
KianaMo <kiana.montazeri@myy.haaga-helia.fi>
2023-02-07 02:56:30 +0200

    add book and delete book

--- src/main/java/fi/haagahelia/bookstore/web/BookController.java
+++ src/main/java/fi/haagahelia/bookstore/web/BookController.java
@@ -10,8 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.ui.Model;
 
 @Controller
@@ -25,7 +27,24 @@ public class BookController {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
 	}
+	@RequestMapping(value = "/add")
+	public String addStudent(Model model){
+	 model.addAttribute("book", new Book());
+	 return "addbook";
+	}
 	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(Book book){
+	 repository.save(book);
+	 return "redirect:booklist";
+	}
+	
+	@RequestMapping(value = "/delete/{id}", 
+			method = RequestMethod.GET)
+	public String deleteBook(@PathVariable("id") Long id, Model model){ 
+		repository.deleteById(id);
+	 return "redirect:../booklist";
+	}
 	@GetMapping("/index")
 	public String bookForm(Model model) {
 		model.addAttribute("books", books);
